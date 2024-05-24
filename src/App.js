a13c62b79ab9cc88b41d80c47e2e7261ff24a78d
achrafameur <achref.ameur1925@gmail.com>
2024-05-24 22:34:11 +0200

    signup page problem fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import AuthProvider from "./auth/AuthContext";
 
 function App() {
-  const navigate = useNavigate();
-  
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      navigate('/login');
-    }
-  }, [navigate]);
 
   return (
     <AuthProvider>
