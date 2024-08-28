5833c02d289c874c89b9fb00c7d5006e2c4afe1e
murkyie <omarbengadha02@gmail.com>
2024-08-28 18:03:47 +0100

    update method

--- src/client/index.js
+++ src/client/index.js
@@ -10,9 +10,9 @@ import {
   Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import altImage from '../../src/imgs/food.png'
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import altImage from "../../src/imgs/food.png";
 
 const ClientDashboard = () => {
   const [menus, setMenus] = useState([]);
@@ -21,8 +21,16 @@ const ClientDashboard = () => {
   useEffect(() => {
     const fetchMenus = async () => {
       try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/professionnel/menus/`
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/professionnel/menus/`,
+          {
+            user_id: userId,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json", // Set content type to JSON
+            },
+          }
         );
         setMenus(response.data);
       } catch (error) {
@@ -48,7 +56,6 @@ const ClientDashboard = () => {
       );
 
       console.log(response.data);
-
     } catch (error) {
       console.error(error);
       // Handle errors
@@ -71,8 +78,7 @@ const ClientDashboard = () => {
       );
 
       console.log(response.data);
-      alert('menu added to chart successfully!')
-
+      alert("menu added to chart successfully!");
     } catch (error) {
       console.error(error);
       // Handle errors
@@ -81,29 +87,30 @@ const ClientDashboard = () => {
 
   return (
     <>
-
       <Container>
-        <div
-          className="pageTitleHeader"
-        >
-          Consulter les menus disponibles
-        </div>
+        <div className="pageTitleHeader">Consulter les menus disponibles</div>
         <Box sx={{ display: "flex", flexWrap: "wrap", gap: "25px" }}>
           {menus.map((menu, index) => (
-            <Card key={menu.id} sx={{ flexBasis: "30%", minWidth: 300 }}
-              style={{ borderRadius: 15, boxShadow: 'rgba(0, 0, 0, 0.04) 0px 5px 22px, rgba(0, 0, 0, 0.03) 0px 0px 0px 0.5px' }}>
+            <Card
+              key={menu.id}
+              sx={{ flexBasis: "30%", minWidth: 300 }}
+              style={{
+                borderRadius: 15,
+                boxShadow:
+                  "rgba(0, 0, 0, 0.04) 0px 5px 22px, rgba(0, 0, 0, 0.03) 0px 0px 0px 0.5px",
+              }}
+            >
               <Link to={`/menu/${menu.id}`}>
                 <CardMedia
                   component="img"
                   height="140"
                   image={
-                    menu.image !== 'image/upload/null'
+                    menu.image !== "image/upload/null"
                       ? `${process.env.REACT_APP_CLOUDINARY_URL}/${menu.image}`
                       : altImage
                   }
                   alt={menu.nom}
                 />
-
               </Link>
               <CardContent>
                 <Typography variant="h5" component="div">
@@ -126,7 +133,10 @@ const ClientDashboard = () => {
                     <Button
                       variant="contained"
                       color="success"
-                      style={{ background: 'linear-gradient(45deg, rgba(57,197,116,1) 14%, rgba(3,162,194,1) 100%)' }}
+                      style={{
+                        background:
+                          "linear-gradient(45deg, rgba(57,197,116,1) 14%, rgba(3,162,194,1) 100%)",
+                      }}
                       onClick={() => addToChart(menu.id)}
                     >
                       <AddShoppingCartIcon />
@@ -138,10 +148,8 @@ const ClientDashboard = () => {
           ))}
         </Box>
       </Container>
-
     </>
   );
 };
 
 export default ClientDashboard;
-
